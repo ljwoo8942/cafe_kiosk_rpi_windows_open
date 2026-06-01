e4aee7bd2c38eaa26d922b4f53d7c3b9100532a1
ljwoo8942 <ljwoo8942@gmail.com>
2026-06-01 19:33:04 +0900

    Find user Inno Setup installs

--- packaging/windows/build_windows_installer.ps1
+++ packaging/windows/build_windows_installer.ps1
@@ -33,6 +33,7 @@ function Find-InnoCompiler {
     }
 
     $candidates = @(
+        "$env:LOCALAPPDATA\Programs\Inno Setup 6\ISCC.exe",
         "${env:ProgramFiles(x86)}\Inno Setup 6\ISCC.exe",
         "$env:ProgramFiles\Inno Setup 6\ISCC.exe"
     )
